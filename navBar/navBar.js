f78c7537007efaa3a8c614f6270b4fd33c6d9616
v_mtonhuang <v_mtonhuang@tencent.com>
2020-12-28 17:33:13 +0800

    feat: update

--- navBar/navBar.js
+++ navBar/navBar.js
@@ -5,11 +5,23 @@
 
 Component({
     properties: {
+        background: { //背景颜色，导航栏
+            type: String,
+            value: '#fff'
+        },
+        color: { //字体颜色
+            type: String,
+            value: 'rgba(0, 0, 0, 1)'
+        },
         titleText: { //导航栏文字
             type: String,
-            value: '导航栏'
+            value: ''
         },
         backgroundImg: { //背景图片路径
+            type: String,
+            value: ''
+        },
+        titleImg: { //标题图片
             type: String,
             value: ''
         },
@@ -20,29 +32,61 @@ Component({
         homeIcon: { //房子按钮
             type: String,
             value: ''
+        },
+        fontSize: { //字体大小
+            type: Number,
+            value: 16
+        },
+        iconHeight: { //icon高度
+            type: Number,
+            value: 19
+        },
+        iconWidth: { //icon宽度
+            type:Number,
+            value: 58
         }
     },
     attached: function(){
-        const self = this;
-        self.getSysmInfo();
+        var that = this;
+        that.setNavSize();
+        that.setStyle();
     },
     data: {
 
     },
     methods: {
-        getSysmInfo: function() {
-            const self = this;
-            wx.getSystemInfo({
-                success: function(res) {
-                    let isIos = res.system.indexOf('iOS') > -1;
-                    let status = res.statusBarHeight;
-                    self.setData({
-                        navHeight: isIos ? 44 : 48,
-                        status: status
-                    })
-                }
+        // 通过获取系统信息计算导航栏高度
+        setNavSize: function() {
+            let that = this, 
+            sysinfo = wx.getSystemInfoSync(), 
+            statusHeight = sysinfo.statusBarHeight, 
+            isiOS = sysinfo.system.indexOf('iOS') > -1,
+            navHeight;
+            if (!isiOS) {
+                navHeight = 48;
+            } else {
+                navHeight = 44;
+            }
+            that.setData({
+                status: statusHeight,
+                navHeight: navHeight
             })
         },
+        setStyle: function() {
+            var that  = this, 
+            containerStyle, 
+            textStyle, 
+            iconStyle;
+            containerStyle = ['background:' + that.data.background].join(';');
+            textStyle = ['color:' + that.data.color,'font-size:' + that.data.fontSize + 'px'].join(';');
+            iconStyle = ['width: ' + that.data.iconWidth + 'px','height: ' + that.data.iconHeight + 'px'].join(';');
+            that.setData({
+                containerStyle: containerStyle,
+                textStyle: textStyle,
+                iconStyle: iconStyle
+            })
+        },
+        // 返回事件
         back: function(){
             this.triggerEvent('back')
         },
@@ -51,3 +95,4 @@ Component({
         }
     }
 })
+
